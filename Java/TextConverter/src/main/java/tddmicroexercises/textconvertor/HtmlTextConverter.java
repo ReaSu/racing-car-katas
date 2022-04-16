cb069eb77c385b483c9f396a18a7a3d2a7c75cca
reasu <reasu@protonmail.com>
2022-04-17 00:43:15 +0200

    HtmlTextConverter can receive text, so testing is independent from file system

--- Java/TextConverter/src/main/java/tddmicroexercises/textconvertor/HtmlTextConverter.java
+++ Java/TextConverter/src/main/java/tddmicroexercises/textconvertor/HtmlTextConverter.java
@@ -4,30 +4,49 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class HtmlTextConverter
-{
-    private String fullFilenameWithPath;
+public class HtmlTextConverter {
+	private String fullFilenameWithPath;
+	private String contents = "";
 
-    public HtmlTextConverter(String fullFilenameWithPath)
-    {
-        this.fullFilenameWithPath = fullFilenameWithPath;
-    }
+	public HtmlTextConverter(String fullFilenameWithPath) {
+		this.fullFilenameWithPath = fullFilenameWithPath;
+	}
 
-    public String convertToHtml() throws IOException{
-    
-	    BufferedReader reader = new BufferedReader(new FileReader(fullFilenameWithPath));
-	    
-	    String line = reader.readLine();
-	    String html = "";
-	    while (line != null)
-	    {
-	    	html += StringEscapeUtils.escapeHtml(line);
-	        html += "<br />";
-	        line = reader.readLine();
-	    }
-	    return html;
+	public HtmlTextConverter(String contents, String fullFilenameWithPath) {
+		this.fullFilenameWithPath = fullFilenameWithPath;
+		this.contents = contents;
+	}
 
-    }
+	public String convertToHtml() throws IOException {
+		if (contents == "") {
+			contents = readFile();
+		}
+		String[] lines = contents.split("\n");
+		String html = "";
+		for (String line : lines) {
+			html += convert(line);
+		}
+		return html;
+	}
+
+	private String convert(String line) {
+		String html = StringEscapeUtils.escapeHtml(line);
+		html += "<br />";
+		return html;
+	}
+
+	private String readFile() throws IOException {
+		String contents = "";
+		BufferedReader reader = new BufferedReader(new FileReader(fullFilenameWithPath));
+		String line = reader.readLine();
+		while (line != null) {
+			contents += line;
+			contents += '\n';
+			line = reader.readLine();
+		}
+		reader.close();
+		return contents;
+	}
 
 	public String getFilename() {
 		return this.fullFilenameWithPath;
--- Java/TextConverter/src/test/java/tddmicroexercises/textconvertor/HtmlTextConverterTest.java
+++ Java/TextConverter/src/test/java/tddmicroexercises/textconvertor/HtmlTextConverterTest.java
@@ -2,14 +2,41 @@ package tddmicroexercises.textconvertor;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.IOException;
+
 import org.junit.jupiter.api.Test;
 
 public class HtmlTextConverterTest {
 
     @Test
-    public void foo() {
-        HtmlTextConverter converter = new HtmlTextConverter("foo");
-        assertEquals("fixme", converter.getFilename());
+    public void readFromFileShouldReturnCorrectFilename() {
+        String fullFilenameWithPath = "/home/reasu/dev/katas/Racing-Car-Katas/Java/TextConverter/src/main/java/resources/textfile.txt";
+		HtmlTextConverter converter = new HtmlTextConverter(fullFilenameWithPath);
+        assertEquals(fullFilenameWithPath, converter.getFilename());
+    }
+    
+    @Test
+    public void passContentsShouldReturnCorrectFileName() {
+    	String contents = "some \"text\" contents\na < b && b > a\n";
+    	String filename = "randomFileName.txt";
+    	
+    	HtmlTextConverter converter = new HtmlTextConverter(contents, filename);
+    	assertEquals(filename, converter.getFilename());
+    }
+    
+    @Test
+    public void passContentsShouldConvertCorrectly() {
+    	String contents = "some \"text\" contents\na < b && b > a\n";
+    	String filename = "randomFileName.txt";
+    	String expected = "some &quot;text&quot; contents<br />a &lt; b &amp;&amp; b &gt; a<br />";
+		String html = "";
+    	HtmlTextConverter converter = new HtmlTextConverter(contents, filename);
+    	try {
+			html = converter.convertToHtml();
+		} catch (IOException e) {
+			e.getMessage();
+		}
+    	assertEquals(html, expected);
     }
 
 }
