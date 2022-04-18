80d761105f8edd55c180bb2a7c65a2e67b86a543
reasu <reasu@protonmail.com>
2022-04-18 18:42:09 +0200

    refactor TelemetryDiagnosticControll for smaller methods.

--- Java/TelemetrySystem/src/main/java/tddmicroexercises/telemetrysystem/TelemetryDiagnosticControls.java
+++ Java/TelemetrySystem/src/main/java/tddmicroexercises/telemetrysystem/TelemetryDiagnosticControls.java
@@ -1,17 +1,54 @@
 package tddmicroexercises.telemetrysystem;
 
 public class TelemetryDiagnosticControls {
-	private final String DiagnosticChannelConnectionString = "*111#";
+	private static final String CONNECTION_STRING = "*111#";
+	private static final String DIAGNOSTIC_MESSAGE = "AT#UD";
 
 	private final TelemetryClient telemetryClient;
 	private String diagnosticInfo = "";
 
-	public TelemetryDiagnosticControls(TelemetryClient tc) {
-		telemetryClient = tc;
+	public TelemetryDiagnosticControls(TelemetryClient telemetryClient) {
+		this.telemetryClient = telemetryClient;
 	}
-	
+
 	public TelemetryDiagnosticControls() {
-		telemetryClient = new TelemetryClient();
+		this.telemetryClient = new TelemetryClient();
+	}
+
+	public void checkTransmission() throws Exception {
+		prepareTransmission();
+		telemetryClient.send(DIAGNOSTIC_MESSAGE);
+		diagnosticInfo = telemetryClient.receive();
+	}
+
+	private void prepareTransmission() throws Exception {
+		resetDiagnosticInfo();
+		resetConnection();
+		connect();
+	}
+
+	private void connect() throws Exception {
+		int retryLeft = 3;
+		while (notConnected() && retryLeft > 0) {
+			telemetryClient.connect(CONNECTION_STRING);
+			retryLeft -= 1;
+		}
+		if (notConnected()) {
+			// this Exception could be a bit more specific.
+			throw new Exception("Unable to connect.");
+		}
+	}
+
+	private void resetConnection() {
+		telemetryClient.disconnect();
+	}
+
+	private void resetDiagnosticInfo() {
+		diagnosticInfo = "";
+	}
+
+	private boolean notConnected() {
+		return telemetryClient.getOnlineStatus() == false;
 	}
 
 	public String getDiagnosticInfo() {
@@ -20,24 +57,5 @@ public class TelemetryDiagnosticControls {
 
 	public void setDiagnosticInfo(String diagnosticInfo) {
 		this.diagnosticInfo = diagnosticInfo;
-	}
-
-	public void checkTransmission() throws Exception {
-		diagnosticInfo = "";
-
-		telemetryClient.disconnect();
-
-		int retryLeft = 3;
-		while (telemetryClient.getOnlineStatus() == false && retryLeft > 0) {
-			telemetryClient.connect(DiagnosticChannelConnectionString);
-			retryLeft -= 1;
-		}
-
-		if (telemetryClient.getOnlineStatus() == false) {
-			throw new Exception("Unable to connect.");
-		}
-
-		telemetryClient.send(TelemetryClient.DIAGNOSTIC_MESSAGE);
-		diagnosticInfo = telemetryClient.receive();
 	}
 }
